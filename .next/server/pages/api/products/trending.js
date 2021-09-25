657c6d769277e891e16fe1b1ad13bf6cdaaf63c5
Karem1986 <karem.ortiz@outlook.com>
2021-09-25 11:45:28 +0200

    Created endpoint products and tested it on Postman

---
+++ .next/server/pages/api/products/trending.js
@@ -0,0 +1,36 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/api/products/trending";
+exports.ids = ["pages/api/products/trending"];
+exports.modules = {
+
+/***/ "./pages/api/products/trending.js":
+/*!****************************************!*\
+  !*** ./pages/api/products/trending.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n//This is the endpoint for our products shop\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (async (req, res) => {\n  return res.status(200).json({\n    data: []\n  });\n}); //After we run the server with npm run dev (script in package.json)//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9hcGkvcHJvZHVjdHMvdHJlbmRpbmcuanMuanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBO0FBQ0EsaUVBQWUsT0FBT0EsR0FBUCxFQUFZQyxHQUFaLEtBQW9CO0FBQ2pDLFNBQU9BLEdBQUcsQ0FBQ0MsTUFBSixDQUFXLEdBQVgsRUFBZ0JDLElBQWhCLENBQXFCO0FBQUVDLElBQUFBLElBQUksRUFBRTtBQUFSLEdBQXJCLENBQVA7QUFDRCxDQUZELEdBR0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zZXJ2ZXIvLi9wYWdlcy9hcGkvcHJvZHVjdHMvdHJlbmRpbmcuanM/Y2U4NCJdLCJzb3VyY2VzQ29udGVudCI6WyIvL1RoaXMgaXMgdGhlIGVuZHBvaW50IGZvciBvdXIgcHJvZHVjdHMgc2hvcFxuZXhwb3J0IGRlZmF1bHQgYXN5bmMgKHJlcSwgcmVzKSA9PiB7XG4gIHJldHVybiByZXMuc3RhdHVzKDIwMCkuanNvbih7IGRhdGE6IFtdIH0pO1xufTtcbi8vQWZ0ZXIgd2UgcnVuIHRoZSBzZXJ2ZXIgd2l0aCBucG0gcnVuIGRldiAoc2NyaXB0IGluIHBhY2thZ2UuanNvbilcbiJdLCJuYW1lcyI6WyJyZXEiLCJyZXMiLCJzdGF0dXMiLCJqc29uIiwiZGF0YSJdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./pages/api/products/trending.js\n");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/api/products/trending.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/api/products/trending.js
@@ -0,0 +1,5 @@
+//This is the endpoint for our products shop
+export default async (req, res) => {
+  return res.status(200).json({ data: [] });
+};
+//After we run the server with npm run dev (script in package.json)
